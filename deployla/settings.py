86f02013ecebe324f1512a4528f48b5168c11119
Lorena <lorenarocio1@gmail.com>
2024-09-23 18:39:57 +0200

    add database settings

--- deployla/settings.py
+++ deployla/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'deployla.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tsuuhtnj_deploy',
+        'USER': "postsuuhtnj_sarcee",
+        'PASSWORD': "treaty7!Alberta",
+        'HOST': "localhost",
+        'PORT': "5432",
     }
 }
 
